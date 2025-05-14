dbd9695679b03eaecc01cb6574509cefc4d439ce
start.vaadin.com <no-reply@vaadin.com>
2025-05-14 13:38:38 +0000

    Generated project

---
+++ src/main/java/at/mci/mick/base/domain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable domain classes.
+ */
+@NullMarked
+package at.mci.mick.base.domain;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/at/mci/mick/base/ui/component/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable UI components.
+ */
+@NullMarked
+package at.mci.mick.base.ui.component;
+
+import org.jspecify.annotations.NullMarked;
